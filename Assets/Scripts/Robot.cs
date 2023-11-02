048db2ed34923a02c5491ab492df60d24dcd2359
Kanika834 <gerasantosh7@gmail.com>
2023-11-02 14:26:42 -0400

    Chapter10 - Added some background Sounds

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Robot : MonoBehaviour
 {
     [SerializeField]
+    private AudioClip deathSound;
+    [SerializeField]
+    private AudioClip fireSound;
+    [SerializeField]
+    private AudioClip weakHitSound;
+
+    [SerializeField]
     GameObject missileprefab;
 
     public Animator robot;
@@ -53,6 +60,7 @@ public class Robot : MonoBehaviour
         missile.transform.position = missileFireSpot.transform.position;
         missile.transform.rotation = missileFireSpot.transform.rotation;
         robot.Play("Fire");
+        GetComponent<AudioSource>().PlayOneShot(fireSound);
     }
 
     public void TakeDamage(int amount)
@@ -67,6 +75,11 @@ public class Robot : MonoBehaviour
             isDead = true;
             robot.Play("Die");
             StartCoroutine("DestroyRobot");
+            GetComponent<AudioSource>().PlayOneShot(deathSound);
+        }
+        else
+        {
+            GetComponent<AudioSource>().PlayOneShot(weakHitSound);
         }
     }
     
